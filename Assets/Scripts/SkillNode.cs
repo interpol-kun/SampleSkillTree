62df99f94c7c67f5b929d56f386dc6ed743d2135
interpol <mrpalich@gmail.com>
2022-10-28 18:45:19 +0300

    Add DFS for articulation points

--- Assets/Scripts/SkillNode.cs
+++ Assets/Scripts/SkillNode.cs
@@ -5,14 +5,17 @@ public class SkillNode : ScriptableObject
 {
     public string skillName = "Skillname";
     public int cost;
-
     public SkillNode[] neighbours;
+
+    public delegate void Changed();
+    public event Changed OnChanged;
     
     private bool _isBought;
 
-    public delegate void Changed();
-
-    public event Changed OnChanged;
+    [HideInInspector] public int num;
+    [HideInInspector] public int low;
+    [HideInInspector] public bool isVisited;
+    [HideInInspector] public SkillNode parent;
 
     public bool HasActiveLink()
     {
@@ -25,38 +28,6 @@ public class SkillNode : ScriptableObject
         }
 
         return false;
-    }
-
-    public bool CanBeForgotten()
-    {
-        return HasActiveLink() && AllNeighboursWontBeOrphans();
-    }
-
-    public bool AllNeighboursWontBeOrphans()
-    {
-        foreach (var skill in neighbours)
-        {
-            if (skill.GetActiveNeighboursCount() < 2)
-            {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
-    public int GetActiveNeighboursCount()
-    {
-        int count = 0;
-        foreach (var skill in neighbours)
-        {
-            if (skill.IsBought())
-            {
-                count++;
-            }
-        }
-
-        return count;
     }
 
     public bool IsBought()
--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class SkillTree : MonoBehaviour
 {
     public SkillNode[] skills;
-
+    public delegate void SkillPointsUpdated(int amount);
+    public static event SkillPointsUpdated OnSkillPointsUpdated;
+    
     [SerializeField] private SkillNode baseSkill;
     [SerializeField] private int maxSkillPoints;
     
-    public delegate void SkillPointsUpdated(int amount);
-
-    public static event SkillPointsUpdated OnSkillPointsUpdated;
-
     private int availableSkillPoints;
-    void Start()
+
+    private List<SkillNode> _cutpoints = new List<SkillNode>();
+    private int _counter;
+
+    private void Start()
     {
         baseSkill.SetBought(true);
         SetAvailableSkillPoints(maxSkillPoints);
@@ -27,7 +32,7 @@ public class SkillTree : MonoBehaviour
 
     public bool CanForget(SkillNode skill)
     {
-        return skill.IsBought() && skill.CanBeForgotten();
+        return skill.IsBought() && !_cutpoints.Contains(skill);
     }
 
     public void Forget(SkillNode skill)
@@ -64,6 +69,97 @@ public class SkillTree : MonoBehaviour
     private void SetAvailableSkillPoints(int amount)
     {
         availableSkillPoints = Mathf.Clamp(amount, 0, maxSkillPoints);
+        FindCutpoints();
         OnSkillPointsUpdated?.Invoke(availableSkillPoints);
+    }
+    
+    //Articulation points functions
+
+    private void FindCutpoints()
+    {
+        _cutpoints.Clear();
+        _counter = 0;
+        foreach (var skill in skills)
+        {
+            skill.isVisited = false;
+        }
+        AssignNum(baseSkill);
+        AssignLow(baseSkill);
+        FindArt(baseSkill);
+    }
+
+    private void AssignNum(SkillNode v)
+    {
+        v.num = _counter++;
+        v.isVisited = true;
+        foreach (var w in v.neighbours)
+        {
+            if (!w.IsBought())
+                continue;
+            
+            if (!w.isVisited)
+            {
+                w.parent = v;
+                AssignNum(w);
+            }
+        }
+    }
+
+    private void AssignLow(SkillNode v)
+    {
+        v.low = v.num;
+        foreach (var w in v.neighbours)
+        {
+            if (!w.IsBought())
+                continue;
+            
+            if (w.num > v.num)
+            {
+                AssignLow(w);
+                if (w.low >= v.num)
+                {
+                    _cutpoints.Add(v);
+                }
+
+                v.low = Mathf.Min(v.low, w.low);
+            }
+            else
+            {
+                if (v.parent != w)
+                {
+                    v.low = Mathf.Min(v.low, w.num);
+                }
+            }
+        }
+    }
+
+    private void FindArt(SkillNode v)
+    {
+        v.isVisited = true;
+        v.low = v.num = _counter++;
+        foreach (var w in v.neighbours)
+        {
+            if (!w.IsBought())
+                continue;
+            
+            if (!w.isVisited)
+            {
+                w.parent = v;
+                FindArt(w);
+                if (w.low >= v.num)
+                {
+                    _cutpoints.Add(v);
+                }
+
+                v.low = Mathf.Min(v.low, w.low);
+            }
+            else
+            {
+                if (v.parent != w)
+                {
+                    v.low = Mathf.Min(v.low, w.num);
+                }
+            }
+        }
     }
 }
