898ed2692e3c5a852354f415f5e6ad0c645a6609
interpol <mrpalich@gmail.com>
2022-10-28 18:46:37 +0300

    Remove unused imports

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngineInternal;
 
 public class SkillTree : MonoBehaviour
 {
